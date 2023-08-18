97283669adb2a0b5872014b05b7fc908d0371b82
Reinaldo Henrique Morais <116378866+RHMestudante@users.noreply.github.com>
2023-08-18 14:38:47 -0300

    Criação de projeto

---
+++ spring/spring/src/main/java/edu/ifsp/ped/spring/PedApplication.java
@@ -0,0 +1,13 @@
+package edu.ifsp.ped.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedApplication.class, args);
+	}
+
+}
---
+++ spring/spring/src/test/java/edu/ifsp/ped/spring/PedApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ifsp.ped.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
